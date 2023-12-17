da84f9575a3b668e08d144584c4c2b5d5e72d9af
Emils Valdmanis <valdmanis71@gmail.com>
2023-12-17 19:01:12 +0200

    Added name for each message

--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -35,7 +35,7 @@ const Messages = () => {
     if(!messages) return <Loading/>
 
     return (
-        <div className="flex flex-col gap-4 text-gray-800 dark:text-gray-100">
+        <div className="flex flex-col gap-8 text-gray-800 dark:text-gray-100">
             {messages.map(message => {
                 const isCurrentUserMsg = uid === message.uid
                 const date = message.createdAt &&
@@ -45,10 +45,13 @@ const Messages = () => {
                     })
 
                 if(date) return (
-                    <div key={message.id} className={classNames('flex items-center gap-4', isCurrentUserMsg && 'flex-row-reverse')}>
-                        <p className="w-10">{date}</p>
-                        <img src={message.avatar} className="w-8 h-8 rounded-full" />
-                        <div className={classNames('px-5 py-1.5 rounded-xl text-gray-100', isCurrentUserMsg ? 'bg-yellow-600' : 'bg-purple-500')}>
+                    <div className={classNames("flex flex-col gap-4", isCurrentUserMsg && 'ml-auto')}>
+                        <div key={message.id} className={classNames('flex items-center gap-4', isCurrentUserMsg && 'flex-row-reverse')}>
+                            <p>{date}</p>
+                            <img src={message.avatar} className="w-8 h-8 rounded-full" />
+                            {!isCurrentUserMsg && <p className="w-32 truncate">{message.name}</p>}
+                        </div>
+                        <div className={classNames('mx-14 px-5 py-1.5 rounded-xl text-gray-100 max-w-xs', isCurrentUserMsg ? 'bg-yellow-600' : 'bg-purple-500')}>
                             {message.text}
                         </div>
                     </div>
