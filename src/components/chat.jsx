b8d22a9bdf6aafd2b62bf45f4d99a61fda979a2e
Emils Valdmanis <valdmanis71@gmail.com>
2023-12-17 18:36:52 +0200

    Overflow scroll

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from "react"
-import classNames from "classnames"
-import MessageInput from "./messageInput"
-import Messages from "./messages"
+import React, { useState } from "react";
+import classNames from "classnames";
+import MessageInput from "./messageInput";
+import Messages from "./messages";
 
 const Chat = () => {
-    return (
-        <div
-            className='container mx-auto grow flex flex-col p-10 bg-gray-400 dark:bg-gray-800 justify-between rounded-3xl shadow-xl gap-8'
-        >
-            <Messages/>
-            <MessageInput/>
-        </div>
-    )
-  }
-  
-  export default Chat
-  
+  return (
+    <div className="container mx-auto grow flex flex-col p-10 bg-gray-400 dark:bg-gray-800 justify-between rounded-3xl shadow-xl gap-8">
+      <div className="overflow-y-auto" style={{height: 'calc(100vh - 16.75rem)'}}>
+        <Messages />
+      </div>
+      <MessageInput />
+    </div>
+  )
+}
+
+export default Chat;
